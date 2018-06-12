aef49b6ea929a5529b7ebe66b58080a3f405ab21
tecmelo <tec_melo@hotmail.com>
2018-06-12 00:33:03 -0500

    forms

--- src/pages/nueva/nueva.component.ts
+++ src/pages/nueva/nueva.component.ts
@@ -11,21 +11,21 @@ export class NuevaComponent implements OnInit {
   listaContactos:any;
   constructor(public modalCtrl: ModalController,
               private contacts: Contacts) {
-
-                this.contacts.find(["*"])
-                   .then(res => {
-                     let datosMostar:any[]=[];
-                     res.map((item) =>{
-                       if(item.displayName != null && item.phoneNumbers != null){
-                         datosMostar.push({displayName:item.displayName,phoneNumbers:item.phoneNumbers})
-                       }
-                     })
-                     this.listaContactos = datosMostar;
-                   },error => {
-                     console.log({error:error})
-                   })
-
-                   console.log(this.listaContactos)
+                //
+                // this.contacts.find(["*"])
+                //    .then(res => {
+                //      let datosMostar:any[]=[];
+                //      res.map((item) =>{
+                //        if(item.displayName != null && item.phoneNumbers != null){
+                //          datosMostar.push({displayName:item.displayName,phoneNumbers:item.phoneNumbers})
+                //        }
+                //      })
+                //      this.listaContactos = datosMostar;
+                //    },error => {
+                //      console.log({error:error})
+                //    })
+                //
+                //    console.log(this.listaContactos)
 
 
 
