bb0c6638e8d0c89f285c8008370d698d48f47ca2
tecmelo <tec_melo@hotmail.com>
2018-06-14 01:14:28 -0500

    Ultimatum

--- src/app/services/contactos.service.ts
+++ src/app/services/contactos.service.ts
@@ -6,17 +6,21 @@ export class ContactosService {
   listaContactos:any;
   constructor(private contacts: Contacts) {
     console.log("Servicio iniciado")
+
+
     this.contacts.find(["*"])
        .then(res => {
+         console.log("res",res)
          let datosMostar:any[]=[];
+         let i=0;
          res.map((item) =>{
-           if(item.displayName != null && item.phoneNumbers != null){
+           if(item.displayName != null && item.phoneNumbers != null && (i<=100)){
              datosMostar.push({displayName:item.displayName,phoneNumbers:item.phoneNumbers})
            }
          })
          this.listaContactos = datosMostar;
        },error => {
-         console.log({error:error})
+         console.log("erro",{error:error})
        })
 
        console.log(this.listaContactos)
