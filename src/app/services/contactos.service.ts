869dcb70480f244a9549c3db8c626b2d935af2c8
tecmelo <tec_melo@hotmail.com>
2018-06-14 09:12:39 -0500

    Ultimatum

--- src/app/services/contactos.service.ts
+++ src/app/services/contactos.service.ts
@@ -1,12 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+import { AlertController } from 'ionic-angular';
+
 
 @Injectable()
 export class ContactosService {
   listaContactos:any=[];
-  constructor(private contacts: Contacts) {
+  constructor(private contacts: Contacts,
+              private alertCtrl: AlertController) {
     console.log("Servicio iniciado")
 
+
+
+
+
+    this.contacts.find(["*"])
+       .then(res => {
+         console.log("res",res)
+         res.map((item) =>{
+           if(item.displayName != null && item.phoneNumbers != null){
+             this.listaContactos.push({displayName:item.displayName,phoneNumbers:item.phoneNumbers})
+           }
+         })
+
+         let alert = this.alertCtrl.create({
+           title: 'Eliminación',
+           message: 'Confirm ',
+           buttons: [
+             {
+               text: 'Cancelar',
+               role: 'cancel'
+             }
+           ]
+         });
+         alert.present();
+
+       },error => {
+         console.log("erro",{error:error})
+       })
 
        console.log(this.listaContactos)
 
@@ -20,6 +51,7 @@ export class ContactosService {
 
 
    consultaContactos(){
-     return this.contacts.find(["*"])     
+
+
    }
 }
